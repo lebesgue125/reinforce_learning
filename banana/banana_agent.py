f5c7b207a780946ca8d74354afbbb589287f16f2
yukai.wu <yukai.wu214@gmail.com>
2018-12-11 17:29:19 -0500

    no message

--- banana/banana_agent.py
+++ banana/banana_agent.py
@@ -2,6 +2,7 @@
 
 from unityagents import UnityEnvironment
 import numpy as np
+from banana.model import QNetwork, Agent
 
 env = UnityEnvironment(file_name="E:\MyWork\gitpython\deep-reinforcement-learning\p1_navigation\Banana_Windows_x86_64\Banana.exe")
 
@@ -9,36 +10,65 @@ env = UnityEnvironment(file_name="E:\MyWork\gitpython\deep-reinforcement-learnin
 brain_name = env.brain_names[0]
 brain = env.brains[brain_name]
 
-# reset the environment
-env_info = env.reset(train_mode=True)[brain_name]
-
-# number of agents in the environment
-print('Number of agents:', len(env_info.agents))
-
-# number of actions
+env_info = env.reset(train_mode=False)[brain_name]
 action_size = brain.vector_action_space_size
-print('Number of actions:', action_size)
-
-# examine the state space
 state = env_info.vector_observations[0]
 print('States look like:', state)
 state_size = len(state)
 print('States have length:', state_size)
 
-env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
-score = 0  # initialize the score
-state = env_info.vector_observations[0]
 
-while True:
-    action = np.random.choice(action_size, p=[0.9, 0.01, 0.01, 0.08])  # select an action
-    print(action)
-    env_info = env.step(action)[brain_name]  # send the action to the environment
-    next_state = env_info.vector_observations[0]  # get the next state
-    reward = env_info.rewards[0]  # get the reward
-    done = env_info.local_done[0]  # see if episode has finished
-    score += reward  # update the score
-    state = next_state  # roll over the state to next time step
-    if done:  # exit loop if episode finished
-        break
+brain_agent = Agent(action_size, state_size, maxMemorySize=10000, gamma=0.95, epsilon=1.0, alpha=0.03, repalce=None)
 
-print("Score: {}".format(score))
+while brain_agent.memCntr < brain_agent.memSize:
+    env_info = env.reset(train_mode=True)[brain_name]
+    done = False
+    count = 0
+    score = 0
+    while not done:
+        action = np.random.choice(action_size, p=[0.4, 0.2, 0.2, 0.2])  # select an action
+        env_info = env.step(action)[brain_name]  # send the action to the environment
+        next_state = env_info.vector_observations[0]  # get the next state
+        reward = env_info.rewards[0]  # get the reward
+        done = env_info.local_done[0]  # see if episode has finished
+        score += reward  # update the score
+        count += 1
+        if done and score < 13:
+            reward -= 100
+        if reward == 0:
+            reward -= 0.5
+        brain_agent.storeTransition(state, action, reward, next_state, done)
+        state = next_state  # roll over the state to next time step
+
+print('done the initializing memory')
+
+scores = []
+epsHistory = []
+numGames = 2000
+batch_size = 200
+
+for i in range(numGames):
+    print('starting game ' + str(i+1), 'epsilon: %.4f' % brain_agent.EPSILON)
+    epsHistory.append(brain_agent.EPSILON)
+    done = False
+    env_info = env.reset(train_mode=True)[brain_name]
+    score = 0
+    state = env_info.vector_observations[0]
+
+    while not done:
+
+        action = brain_agent.chooseAction(state)
+        env_info = env.step(action)[brain_name]  # send the action to the environment
+        next_state = env_info.vector_observations[0]  # get the next state
+        reward = env_info.rewards[0]  # get the reward
+        done = env_info.local_done[0]  # see if episode has finished
+        score += reward
+        if done and score < 13:
+            reward -= 100
+        if reward == 0:
+            reward -= 0.5
+        brain_agent.storeTransition(state, action, reward, next_state, done)
+        state = next_state
+    brain_agent.learn(batch_size)
+    scores.append(score)
+    print("Score: {}".format(score))
